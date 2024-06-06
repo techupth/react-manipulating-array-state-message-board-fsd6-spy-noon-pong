04404ee5316b13911f96b8fbb3e64d573d9d2f82
NoonMira <163841273+NoonMira@users.noreply.github.com>
2024-06-06 14:18:59 +0700

    feat: array state

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,57 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [message, setMessage] = useState("");
+  const [array, setArray] = useState([]);
+
+  const addMessage = (event) => {
+    event.preventDefault();
+    const newMessage = [...array];
+    newMessage.push(message);
+    setArray(newMessage);
+  };
+  const deleteData = (index) => {
+    const data = [...array];
+    data.splice(index, 1);
+    setArray(data);
+  };
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <form class="message-input-container" onSubmit={addMessage}>
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
             placeholder="Enter message here"
           />
         </label>
-        <button className="submit-message-button">Submit</button>
-      </div>
+        <button className="submit-message-button" type="submit">
+          Submit
+        </button>
+      </form>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {array.map((item, index) => {
+          return (
+            <div className="message">
+              <h1>{item}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  deleteData(index);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
